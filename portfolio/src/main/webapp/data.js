80d7de84d446414453dc58f5b55c66e23855c1d1
Grace Tian <gracetian6@gmail.com>
2020-05-29 14:32:16 -0400

    added typing, changed homepage

--- portfolio/src/main/webapp/data.js
+++ portfolio/src/main/webapp/data.js
@@ -1,66 +1,24 @@
 // arrays store text that will be displayed on pages 
 window.moments = [
-    `I did math research on knots in high school, 
-    so my goal was to prove new patterns about these 
-    geometrical figures. So, I spent a lot of time 
-    drawing a lot of diagrams to test out new ideas.`,
+    `I did math research on knots in high school, so my goal was to prove new patterns about these geometrical figures. So, I spent a lot of time drawing a lot of diagrams to test out new ideas.`,
 
-    `I went to the Boston Harbor Islands during a summer camp. 
-    This was one of the worst beaches I've been to - 
-    the beaches were rocky and it was painful walking across it. 
-    So many of us got sunburns that day and my friend had to 
-    run to the medical center to get aloe vera gel.`,
+    `I went to the Boston Harbor Islands during a summer camp. This was one of the worst beaches I've been to - the beaches were rocky and it was painful walking across it. So many of us got sunburns that day and my friend had to run to the medical center to get aloe vera gel.`,
 
-    `In a summer camp in my junior year in high school, 
-     all 80 of us were forced to go out on the lawns of 
-     MIT on a piece of tarp for 4th of july bonding. The 
-     sprinklers went off at five in the morning, and we 
-     were forced to get on the sidewalk. My friend and I 
-     were annoyed our sleep got disrupted, so we continued 
-     it on the sidewalk. `,
+    `In a summer camp in my junior year in high school, all 80 of us were forced to go out on the lawns of MIT on a piece of tarp for 4th of july bonding. The sprinklers went off at five in the morning, and we were forced to get on the sidewalk. My friend and I were annoyed our sleep got disrupted, so we continued it on the sidewalk. `,
 
-    `I was quite burned out after my summer camp in junior year. 
-    I was worried about colleges and had little motivation. 
-    My family went on a walk on this nature trial and I felt 
-    much more relaxed afterwards.`, 
+    `I was quite burned out after my summer camp in junior year. I was worried about colleges and had little motivation. My family went on a walk on this nature trial and I felt much more relaxed afterwards.`, 
 
-    `I was at the Beijing airport at around 5 or 6 AM, 
-    and I was waiting for a flight to Hong Kong. 
-    Then, I got an update from Harvard early admissions 
-    and got in! I was excited for the rest of the week 
-    in Hong Kong.`,
+    `I was at the Beijing airport at around 5 or 6 AM, and I was waiting for a flight to Hong Kong. Then, I got an update from Harvard early admissions and got in! I was excited for the rest of the week in Hong Kong.`,
 
-    `I didn't have many friends when I did research at 
-    the University of Minnesota, but I met a really good 
-    friend I still talk to today. She invited me to her
-    family's cabin up north and we picked wild blueberries. 
-    It was humid, the mosquitoes bit me so much that I lost 
-    count, but it was a fond trip I'll always remember.`,
+    `I didn't have many friends when I did research at the University of Minnesota, but I met a really good friend I still talk to today. She invited me to her family's cabin up north and we picked wild blueberries. It was humid, the mosquitoes bit me so much that I lost count, but it was a fond trip I'll always remember.`,
 
-    `My friend and I were checking statistics homework at 
-    2 or 3 AM. I remember we had a good talk. I was tired 
-    so I stared at the ceiling in exasperation and took this pic.`,
+    `My friend and I were checking statistics homework at 2 or 3 AM. I remember we had a good talk. I was tired so I stared at the ceiling in exasperation and took this pic.`,
 
+    `I went to the Grace Hopper conference in Orlando, and I got treated to Universal Studios. It was a very magical time witnessing Harry Potter come to life, and in that moment I honestly felt so lucky to be alive.`,
 
-    `I went to the Grace Hopper conference in Orlando, 
-    and I got treated to Universal Studios. It was a 
-    very magical time witnessing Harry Potter come to 
-    life, and in that moment I honestly felt so lucky 
-    to be alive.`,
+    `I went up north in Minnesota over the winter and we decided to go for a nice winter walk. It got really exhausting so I often lied down on the snow (highly recommend).`,
 
-    `I went up north in Minnesota over the winter and 
-    we decided to go for a nice winter walk. It got 
-    really exhausting so I often lied down on the snow. 
-    I highly recommend since it was so zen and peaceful 
-    out here in nature.`,
+    `I used to enjoy drawing when I was little, but gave up because coloring was too hard. My friend was teaching me how to add color. She demonstrated how an orange object reflects orange light. `,
 
-    `I used to enjoy drawing when I was little, but 
-    gave up because coloring was too hard. My friend 
-    was teaching me how to add color. She demonstrated 
-    how an orange object reflects orange light. `,
-
-    `I have a food gram and was anxious to get a good 
-    picture while I was at Disney World. We stopped by
-    starbucks, and I found the money shot right here out 
-    in the streets. `,
+    `I have a food gram and was anxious to get a good picture while I was at Disney World. We stopped by starbucks, and I found the money shot right here out in the streets. `,
 ]
