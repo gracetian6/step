d203be6160e5f14ee2369cdea40e1c4da794fa4c
Grace Tian <gracetian6@gmail.com>
2020-06-09 19:08:41 +0000

    implemented login servlet

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -23,7 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
-public class HomeServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
