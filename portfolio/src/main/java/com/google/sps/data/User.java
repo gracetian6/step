e6a9d49aaa476957e2540ede7e29877488e14d5f
Grace Tian <gracetian6@gmail.com>
2020-06-10 19:29:40 +0000

    added User class

---
+++ portfolio/src/main/java/com/google/sps/data/User.java
@@ -0,0 +1,11 @@
+package com.google.sps.data;
+
+/** An item on a todo list. */
+public final class User {
+
+  private final boolean loginStatus;
+
+  public User(boolean loginStatus) {
+    this.loginStatus = loginStatus;
+  }
+}
