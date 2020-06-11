d73fcb6fff8b8e21ae1e7ba6a535429f66ba2005
Grace Tian <gracetian6@gmail.com>
2020-06-11 21:28:52 +0000

    login text changes to say logout

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -77,9 +77,11 @@ function getComments() {
  */
 function initComments() {
   fetch(`/loginStatus`).then(response => response.json()).then((msg) => {
+    console.log(msg);
     if (msg.loginStatus == false) {
       document.getElementById("commentForm").style.display="none";
     } else {
+      document.getElementById("loginLink").innerHTML= `<a href="login">Logout here</a> to hide comments:`;
       document.getElementById("commentForm").style.display="block";
       // extract numComment from URL 
       var url = new URL(document.URL);
