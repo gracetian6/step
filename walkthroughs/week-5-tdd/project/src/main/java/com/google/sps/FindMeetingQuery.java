29cce0ccf8f76aba4d3106b95659e9f115aea92c
Grace Tian <gracetian6@gmail.com>
2020-06-19 16:52:02 +0000

    pseudo code: intervals btwn conflicts

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -21,26 +21,55 @@ import java.util.Collections;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-
+    
     // find conflicts of all required attendees
-    Collection<TimeRange> conflicts = new ArrayList<>();
-    int num_attendees = request.getAttendees().size(); //check pseudo code 
+    ArrayList<TimeRange> conflicts = new ArrayList<>();
+    ArrayList<TimeRange> free = new ArrayList<>();
+
     for (Event event : events) {
-      for (int j = 0; j < num_attendees; j++) {
-        if (event == request.getAttendees()[j]){
+      for (String attendee : request.getAttendees()) {
+        if (event.getAttendees().contains(attendee)) {
           conflicts.add(event.getWhen());
         }
       }
     }
 
 
+    System.out.println("PRINTING COLLECTIONS: ");
+    System.out.println(Arrays.toString(conflicts.toArray()));
+
     // sort events in O(nlogn) time
     Collections.sort(conflicts, TimeRange.ORDER_BY_START);
 
     // modify events so that all events are disjoint
+    for (TimeRange conflict1 : conflicts) {
+      for (TimeRange conflict2 : conflicts) {
+        // pop first and then get end of next one that overlaps -- 
+        // keeping going until no more overlap
+      }
+    }
 
-    //find intervals between all disjoint events 
+    //find intervals between events 
+      // pop next item 
+      // keep searching until next event doesn't overlap
+    num_conflicts = conflicts.size(); // check 
+    int start = 0;
+    while (start < num_conflicts){
+      
+      bolean overlap = 
+      // keep searching until next event doesn't overlap
+      for (int end = start + 1; end < num_conflicts; end++) {
+        // if conflicts.get(end) doesn't overlap with conflicts.get(start):
+          // then add the interval of conflicts.get(end-1).end and conflicts.get(end).start
+          // and reset
+            // start = end
+      }
+      
+
+    } 
 
     //throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    return conflicts;
   }
 }
